b6b4314b77352e8f5e04a9d5a79ede5cc0a2931a
amarianjel <abrm.enginner@gmail.com>
2023-11-23 16:06:54 -0300

    if ( authStatus == AuthStatus.authenticated ) {

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,32 +1,64 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:teslo_shop/features/auth/auth.dart';
 import 'package:teslo_shop/features/products/products.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/splash',
-  routes: [
+import '../../features/auth/presentation/providers/providers.dart';
+import 'app_router_notifier.dart';
 
-    //* Primera ruta
-    GoRoute(
-      path: '/splash',
-      builder: (context, state) => const CheckAuthStatusScreen(),
-    ),
+final goRouterProvider = Provider((ref){
 
-    ///* Auth Routes
-    GoRoute(
-      path: '/login',
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      path: '/register',
-      builder: (context, state) => const RegisterScreen(),
-    ),
+  final goRouterNotifier = ref.read( goRouterNotifierProvider );
 
-    ///* Product Routes
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const ProductsScreen(),
-    ),
-  ],
-  ///! TODO: Bloquear si no se está autenticado de alguna manera
-);
+  return GoRouter(
+    initialLocation: '/splash',
+    refreshListenable: goRouterNotifier,
+    routes: [
+
+      //* Primera ruta
+      GoRoute(
+        path: '/splash',
+        builder: (context, state) => const CheckAuthStatusScreen(),
+      ),
+
+      ///* Auth Routes
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: '/register',
+        builder: (context, state) => const RegisterScreen(),
+      ),
+
+      ///* Product Routes
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const ProductsScreen(),
+      ),
+    ],
+    ///README: Bloquear si no se está autenticado de alguna manera
+    redirect: (context, state) {
+      
+      final isGoingTo = state.subloc;
+      final authStatus = goRouterNotifier.authStatus;
+      // print('GoRouter authStatus: $authStatus, isGoingTo: $isGoingTo');
+
+      if ( isGoingTo == '/splash' && authStatus == AuthStatus.checking ) return '/login';
+
+      if ( authStatus == AuthStatus.notAuthenticated ) {
+        if ( isGoingTo == '/login' || isGoingTo == '/register' ) return null;
+
+        return '/login';
+      }
+
+      if ( authStatus == AuthStatus.authenticated ) {
+        if ( isGoingTo == '/login' || isGoingTo == '/register' || isGoingTo == '/splash' ){
+          return '/';
+        }
+      }
+
+      return null;
+    },
+  );
+});
