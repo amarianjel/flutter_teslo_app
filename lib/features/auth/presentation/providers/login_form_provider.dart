d86e4a18a380303029d8f9193401315c7e0d8d40
amarianjel <abrm.enginner@gmail.com>
2023-11-23 16:13:00 -0300

    Bloquear botón de login

--- lib/features/auth/presentation/providers/login_form_provider.dart
+++ lib/features/auth/presentation/providers/login_form_provider.dart
@@ -78,9 +78,13 @@ class LoginFormNotifier extends StateNotifier<LoginFormState> {
   onFormSubmit() async {
     _touchEveryField();
 
-    if( !state.isValid ) return;
+    if ( !state.isValid ) return;
+
+    state = state.copyWith(isPosting: true);
 
     await loginUserCallback( state.email.value, state.password.value );
+
+    state = state.copyWith(isPosting: false);
   }
 
   _touchEveryField(){
